24a217da7113ccd58410b569d63f480f5a06cd11
117Liam <164023705+117Liam@users.noreply.github.com>
2024-04-04 19:10:44 -0300

    Add files via upload

---
+++ Vetor12.java
@@ -0,0 +1,12 @@
+// Durante a contagem de produtos em um estoque, é necessário identificar os produtos com quantidades pares. Faça um programa que imprima todos os números pares de 1 a 50, que representam a contagem de produtos, utilizando um loop for.
+
+public class Vetor12 {
+    public static void main(String[] args) {
+        // Imprimir os números pares de 1 a 50
+        System.out.println("Números pares de 1 a 50:");
+
+        for (int i = 2; i <= 50; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
